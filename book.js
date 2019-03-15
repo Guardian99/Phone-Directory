3fb8027759141b8dda5a94e38f4bc9dee1e233f3
Kanishk Rana <32814336+Guardian99@users.noreply.github.com>
2019-03-15 22:20:14 +0530

    Update book.js

--- book.js
+++ book.js
@@ -1,6 +1,7 @@
-// Book Class: Represents a Book
-class Book {
-  constructor(firstname, lastname, number) {
+class Book 
+{
+  constructor(firstname, lastname, number) 
+  {
     this.firstname = firstname;
     this.lastname = lastname;
     this.number = number;
@@ -9,34 +10,34 @@ class Book {
 
 // UI Class: Handle UI Tasks
 class UI {
-  static displayBooks() {
+  static displayBooks() 
+  {
     const books = Store.getBooks();
-
     books.forEach((book) => UI.addBookToList(book));
   }
 
   static addBookToList(book) {
     const list = document.querySelector('#book-list');
-
     const row = document.createElement('tr');
-
     row.innerHTML = `
       <td>${book.firstname}</td>
       <td>${book.lastname}</td>
       <td>${book.number}</td>
       <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
     `;
-
     list.appendChild(row);
   }
 
-  static deleteBook(el) {
-    if(el.classList.contains('delete')) {
+  static deleteBook(el) 
+  {
+    if(el.classList.contains('delete')) 
+    {
       el.parentElement.parentElement.remove();
     }
   }
 
-  static showAlert(message, className) {
+  static showAlert(message, className) 
+  {
     const div = document.createElement('div');
     div.className = `alert alert-${className}`;
     div.appendChild(document.createTextNode(message));
@@ -48,7 +49,8 @@ class UI {
     setTimeout(() => document.querySelector('.alert').remove(), 3000);
   }
 
-  static clearFields() {
+  static clearFields() 
+  {
     document.querySelector('#firstname').value = '';
     document.querySelector('#lastname').value = '';
     document.querySelector('#number').value = '';
@@ -56,29 +58,38 @@ class UI {
 }
 
 // Store Class: Handles Storage
-class Store {
-  static getBooks() {
+class Store 
+{
+  static getBooks() 
+  {
     let books;
-    if(localStorage.getItem('books') === null) {
+    if(localStorage.getItem('books') === null) 
+    {
       books = [];
-    } else {
+    } 
+    else 
+    {
       books = JSON.parse(localStorage.getItem('books'));
     }
 
     return books;
   }
 
-  static addBook(book) {
+  static addBook(book) 
+  {
     const books = Store.getBooks();
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
   }
 
-  static removeBook(number) {
+  static removeBook(number) 
+  {
     const books = Store.getBooks();
 
-    books.forEach((book, index) => {
-      if(book.number === number) {
+    books.forEach((book, index) => 
+    {
+      if(book.number === number) 
+      {
         books.splice(index, 1);
       }
     });
@@ -91,7 +102,8 @@ class Store {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
-document.querySelector('#book-form').addEventListener('submit', (e) => {
+document.querySelector('#book-form').addEventListener('submit', (e) => 
+{
   // Prevent actual submit
   e.preventDefault();
 
@@ -101,9 +113,12 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   const number = document.querySelector('#number').value;
 
   // Validate
-  if(number === '') {
-    UI.showAlert('Please fill in a valid phone number', 'danger');
-  } else {
+  if(number === '') 
+  {
+    UI.showAlert('Please fill a phone number', 'danger');
+  } 
+  else 
+  {
     // Instatiate book
     const book = new Book(firstname, lastname, number);
 
@@ -114,7 +129,7 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     Store.addBook(book);
 
     // Show success message
-    UI.showAlert('Phone Number Added', 'success');
+    UI.showAlert('Number Added To PhoneBook', 'success');
 
     // Clear fields
     UI.clearFields();
@@ -130,5 +145,5 @@ document.querySelector('#book-list').addEventListener('click', (e) => {
   Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // Show success message
-  UI.showAlert('Phone Number Deleted', 'success');
+  UI.showAlert('Number Deleted', 'success');
 });
